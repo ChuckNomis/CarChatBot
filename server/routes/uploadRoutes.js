0febb4d9018529248e809fa9a6c9cbd15a66d3ba
NadavSi <52112772+ChuckNomis@users.noreply.github.com>
2025-04-29 15:12:25 +0300

    Update uploadRoutes.js

--- server/routes/uploadRoutes.js
+++ server/routes/uploadRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { uploadBook } = require('../controllers/uploadController');
 const router = express.Router();
-
 router.post('/upload', uploadBook);
 
 module.exports = router;
